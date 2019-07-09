5f607e0f58fbbd22e2f60443da81662e936354f4
daan <daanl@outlook.com>
2019-07-08 17:15:34 -0700

    update documentation

--- docs/group__aligned.js
+++ docs/group__aligned.js
@@ -6,10 +6,6 @@ var group__aligned =
     [ "mi_malloc_aligned_at", "group__aligned.html#ga5850da130c936bd77db039dcfbc8295d", null ],
     [ "mi_realloc_aligned", "group__aligned.html#ga4028d1cf4aa4c87c880747044a8322ae", null ],
     [ "mi_realloc_aligned_at", "group__aligned.html#gaf66a9ae6c6f08bd6be6fb6ea771faffb", null ],
-    [ "mi_recalloc_aligned", "group__aligned.html#gabc3dd1f77cf989b9c22862b10b95a89a", null ],
-    [ "mi_recalloc_aligned_at", "group__aligned.html#gadd457d58f04a8bdfaf91d3b91911bb14", null ],
-    [ "mi_rezalloc_aligned", "group__aligned.html#gacd71a7bce96aab38ae6de17af2eb2cf0", null ],
-    [ "mi_rezalloc_aligned_at", "group__aligned.html#gae8b358c417e61d5307da002702b0a8e1", null ],
     [ "mi_zalloc_aligned", "group__aligned.html#ga0cadbcf5b89a7b6fb171bc8df8734819", null ],
     [ "mi_zalloc_aligned_at", "group__aligned.html#ga5f8c2353766db522565e642fafd8a3f8", null ]
 ];
