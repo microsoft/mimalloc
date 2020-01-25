7785139201dbac8bc9515d7f5fa148f3e0c7827d
daan <daanl@outlook.com>
2020-01-25 13:28:24 -0800

    fix warning on gcc on attribute ignore in templates

--- src/arena.c
+++ src/arena.c
@@ -54,7 +54,7 @@ bool  _mi_os_commit(void* p, size_t size, bool* is_zero, mi_stats_t* stats);
 #define MI_MAX_ARENAS         (64)                     // not more than 256 (since we use 8 bits in the memid)
 
 // A memory arena descriptor
-typedef mi_decl_cache_align struct mi_arena_s {
+typedef struct mi_arena_s {
   _Atomic(uint8_t*) start;                // the start of the memory area
   size_t   block_count;                   // size of the area in arena blocks (of `MI_ARENA_BLOCK_SIZE`)
   size_t   field_count;                   // number of bitmap fields (where `field_count * MI_BITMAP_FIELD_BITS >= block_count`)
