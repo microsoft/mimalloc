67de2549cf8585250e17501e714c83a21746b20b
daan <daanl@outlook.com>
2020-02-14 09:40:56 -0800

    fix build with clang-cl due to _Check_return_ (issue #200)

--- include/mimalloc.h
+++ include/mimalloc.h
@@ -26,9 +26,9 @@ terms of the MIT license. A copy of the license can be found in the file
 
 #if (__cplusplus >= 201703)
   #define mi_decl_nodiscard    [[nodiscard]]
-#elif (__GNUC__ >= 4)         
+#elif (__GNUC__ >= 4) || defined(__clang__)  // includes clang, icc, and clang-cl
   #define mi_decl_nodiscard    __attribute__((warn_unused_result))
-#elif (_MSC_VER >= 1700)
+#elif (_MSC_VER >= 1700) 
   #define mi_decl_nodiscard    _Check_return_
 #else 
   #define mi_decl_nodiscard 
