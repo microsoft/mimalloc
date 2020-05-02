01ad5539780b4696b734eec964dbc59bacb2c45d
daan <daanl@outlook.com>
2020-05-02 00:13:03 -0700

    set default reset delay to 250ms

--- src/options.c
+++ src/options.c
@@ -80,7 +80,7 @@ static mi_option_desc_t options[_mi_option_last] =
   { 1, UNINIT, MI_OPTION(eager_commit_delay) },  // the first N segments per thread are not eagerly committed
 #endif
   { 1,    UNINIT, MI_OPTION(allow_decommit) },    // decommit pages when not eager committed
-  { 100,  UNINIT, MI_OPTION(reset_delay) },       // reset delay in milli-seconds
+  { 250,  UNINIT, MI_OPTION(reset_delay) },       // reset delay in milli-seconds
   { 1000, UNINIT, MI_OPTION(arena_reset_delay) }, // reset delay in milli-seconds
   { 0,    UNINIT, MI_OPTION(use_numa_nodes) },    // 0 = use available numa nodes, otherwise use at most N nodes. 
   { 100,  UNINIT, MI_OPTION(os_tag) },            // only apple specific for now but might serve more or less related purpose
