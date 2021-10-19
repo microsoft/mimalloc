10c31f9b41f5559e459dc30cf463cc750bfa67ac
Daan Leijen <daan@microsoft.com>
2021-10-19 15:13:01 -0700

    fix warnings

--- test/test-api.c
+++ test/test-api.c
@@ -4,6 +4,7 @@ This is free software; you can redistribute it and/or modify it under the
 terms of the MIT license. A copy of the license can be found in the file
 "LICENSE" at the root of this distribution.
 -----------------------------------------------------------------------------*/
+#pragma GCC diagnostic ignored "-Walloc-size-larger-than="
 
 /*
 Testing allocators is difficult as bugs may only surface after particular
