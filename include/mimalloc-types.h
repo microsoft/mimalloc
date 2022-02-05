0e2df71829597295c94426539d00c08414d2725b
daan <daanl@outlook.com>
2022-02-04 16:11:38 -0800

    increase minimal commit size to 8*slice-size and add decommit_extend_delay as option

--- include/mimalloc-types.h
+++ include/mimalloc-types.h
@@ -320,10 +320,10 @@ typedef enum mi_segment_kind_e {
 // the corresponding MI_COMMIT_SIZE area is committed.
 // The MI_COMMIT_SIZE must be a multiple of the slice
 // size. If it is equal we have the most fine grained 
-// decommit (but in practice 2x seems to perform better).
+// decommit but setting it higher can be more efficient.
 // ------------------------------------------------------
 
-#define MI_COMMIT_SIZE              (MI_SEGMENT_SLICE_SIZE)   
+#define MI_COMMIT_SIZE              (8*MI_SEGMENT_SLICE_SIZE)   
 #define MI_COMMIT_MASK_BITS         (MI_SEGMENT_SIZE / MI_COMMIT_SIZE)  
 #define MI_COMMIT_MASK_FIELD_BITS    MI_SIZE_BITS
 #define MI_COMMIT_MASK_FIELD_COUNT  (MI_COMMIT_MASK_BITS / MI_COMMIT_MASK_FIELD_BITS)
