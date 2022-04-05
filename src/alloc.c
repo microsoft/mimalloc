12e54665142c771a43f9365474254232764442f6
Jarred Sumner <jarred@jarredsumner.com>
2022-04-05 02:23:06 -0700

    Support `-fno-exceptions`

--- src/alloc.c
+++ src/alloc.c
@@ -802,9 +802,11 @@ static bool mi_try_new_handler(bool nothrow) {
   #endif  
   if (h==NULL) {
     _mi_error_message(ENOMEM, "out of memory in 'new'");      
-    if (!nothrow) {
+#ifdef __EXCEPTIONS
       throw std::bad_alloc();
-    }
+#else 
+      abort();
+#endif
     return false;
   }
   else {
