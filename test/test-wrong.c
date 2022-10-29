093724bdef6511a41a86051e04fff79b572709ca
daan <daan@effp.org>
2022-10-28 20:07:31 -0700

    add test file for valgrind integration

---
+++ test/test-wrong.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "mimalloc.h"
+
+#ifdef USE_STD_MALLOC
+# define mi(x) x
+#else
+# define mi(x) mi_##x
+#endif
+
+int main(int argc, char** argv) {
+  int* p = mi(malloc)(3*sizeof(int));
+  int* q = mi(malloc)(sizeof(int));
+  
+  // undefined access
+  // printf("undefined: %d\n", *q);
+  
+  *q = 42;
+
+  // buffer overflow
+  // q[1] = 43;
+  
+  mi(free)(q);
+
+  // double free
+  mi(free)(q);
+
+  // use after free
+  printf("use-after-free: %d\n", *q);
+
+  // leak p
+  // mi_free(p)
+  return 0;  
+}
