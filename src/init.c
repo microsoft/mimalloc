27fc2cf17e40fd28ba952c1dfad5d17abdf1cb56
daan <daan@effp.org>
2022-11-07 14:57:07 -0800

    fix random initializer

--- src/init.c
+++ src/init.c
@@ -95,7 +95,7 @@ mi_decl_cache_align const mi_heap_t _mi_heap_empty = {
   0,                // tid
   0,                // cookie
   { 0, 0 },         // keys
-  { {0}, {0}, 0 },
+  { {0}, {0}, 0, true }, // random
   0,                // page count
   MI_BIN_FULL, 0,   // page retired min/max
   NULL,             // next
@@ -127,7 +127,7 @@ mi_heap_t _mi_heap_main = {
   0,                // thread id
   0,                // initial cookie
   { 0, 0 },         // the key of the main heap can be fixed (unlike page keys that need to be secure!)
-  { {0x846ca68b}, {0}, 0 },  // random
+  { {0x846ca68b}, {0}, 0, true },  // random
   0,                // page count
   MI_BIN_FULL, 0,   // page retired min/max
   NULL,             // next heap
