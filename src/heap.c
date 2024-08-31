13490948d13530b10b81deb803a2c94d41f05a2b
Zhihua Lai <zhihualai@microsoft.com>
2024-08-31 20:47:37 +0100

    Fix potential buffer overflow warning

--- src/heap.c
+++ src/heap.c
@@ -59,7 +59,8 @@ static bool mi_heap_page_is_valid(mi_heap_t* heap, mi_page_queue_t* pq, mi_page_
   MI_UNUSED(pq);
   mi_assert_internal(mi_page_heap(page) == heap);
   mi_segment_t* segment = _mi_page_segment(page);
-  mi_assert_internal(segment->thread_id == heap->thread_id);
+  uintptr_t thread_id = __atomic_load_n(&segment->thread_id, __ATOMIC_RELAXED);
+  mi_assert_internal(thread_id == heap->thread_id);
   mi_assert_expensive(_mi_page_is_valid(page));
   return true;
 }
