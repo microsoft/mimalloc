061ef80de7c6240c756c6786f73dfbfeba2e006c
Daan Leijen <daan@microsoft.com>
2025-01-07 21:39:11 -0800

    clarify allow_destroy

--- src/init.c
+++ src/init.c
@@ -259,6 +259,7 @@ static void mi_heap_main_init(void) {
     //heap_main.keys[0] = _mi_heap_random_next(&heap_main);
     //heap_main.keys[1] = _mi_heap_random_next(&heap_main);
     _mi_heap_guarded_init(&heap_main);
+    heap_main.allow_page_reclaim = mi_option_is_enabled(mi_option_reclaim_on_free);
     heap_main.allow_page_abandon = (mi_option_get(mi_option_page_full_retain) >= 0);
     heap_main.full_page_retain   = mi_option_get_clamp(mi_option_page_full_retain, -1, 32);
   }
