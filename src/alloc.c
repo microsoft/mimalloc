b54a7d836c21a79bda04be3112c113841ca24dec
Daan <daanl@outlook.com>
2025-03-02 17:41:13 -0800

    fix compilation error with MI_GUARDED

--- src/alloc.c
+++ src/alloc.c
@@ -666,9 +666,9 @@ mi_decl_restrict void* _mi_heap_malloc_guarded(mi_heap_t* heap, size_t size, boo
   if (p != NULL) {
     if (!mi_heap_is_initialized(heap)) { heap = mi_prim_get_default_heap(); }
     #if MI_STAT>1
-    mi_heap_stat_increase(heap, malloc, mi_usable_size(p));
+    mi_heap_stat_increase(heap, malloc_requested, mi_usable_size(p));
     #endif
-    _mi_stat_counter_increase(&heap->tld->stats.guarded_alloc_count, 1);
+    _mi_stat_counter_increase(&heap->tld->stats.malloc_guarded_count, 1);
   }
   #if MI_DEBUG>3
   if (p != NULL && zero) {
