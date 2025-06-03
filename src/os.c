6c3d75a355c14bd3e67c67aed76f0297ab24ed6f
Daan <daanl@outlook.com>
2025-06-03 15:07:15 -0700

    fix base address if commit fails on aligned overallocation

--- src/os.c
+++ src/os.c
@@ -301,7 +301,7 @@ static void* mi_os_prim_alloc_aligned(size_t size, size_t alignment, bool commit
       // explicitly commit only the aligned part
       if (commit) {
         if (!_mi_os_commit(p, size, NULL)) {
-          mi_os_prim_free(p, over_size, 0);
+          mi_os_prim_free(*base, over_size, 0);
           return NULL;
         }
       }
