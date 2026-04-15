d13fd0447ab8c7401c069557ee607dfb5191baa9
Daan <daanl@outlook.com>
2026-04-15 15:04:31 -0700

    prepare for release, bump version to v1.9.8

--- include/mimalloc.h
+++ include/mimalloc.h
@@ -8,7 +8,7 @@ terms of the MIT license. A copy of the license can be found in the file
 #ifndef MIMALLOC_H
 #define MIMALLOC_H
 
-#define MI_MALLOC_VERSION 197   // major + 2 digits minor
+#define MI_MALLOC_VERSION 10908   // major + 2 digits minor + 2 digits patch
 
 // ------------------------------------------------------
 // Compiler specific attributes
