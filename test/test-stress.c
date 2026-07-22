f0cdc11aa90aa399a5da56a2cf9698bd8fdb4cf5
daanx <daan@microsoft.com>
2026-07-22 09:07:04 -0700

    update copyright year

--- test/test-stress.c
+++ test/test-stress.c
@@ -1,5 +1,5 @@
 /* ----------------------------------------------------------------------------
-Copyright (c) 2018-2025 Microsoft Research, Daan Leijen
+Copyright (c) 2018-2026 Microsoft Research, Daan Leijen
 This is free software; you can redistribute it and/or modify it under the
 terms of the MIT license.
 -----------------------------------------------------------------------------*/
