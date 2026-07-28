730199fe1b19d823cae0dfcc731b32fd19d8bc23
daanx <daan@microsoft.com>
2026-07-27 18:24:16 -0700

    define env var MI_TEST_LIGHT reduce test load

--- test/test-stress-heaps.c
+++ test/test-stress-heaps.c
@@ -7,7 +7,7 @@ terms of the MIT license.
 #define TEST_STRESS          1
 #define MI_USE_HEAPS         4
 
-#if !defined(MI_QEMU)  // too slow in test integration
+#if !defined(MI_TEST_LIGHT)  // too slow in test integration
 #define ALLOW_LARGE          1
 #endif
 
