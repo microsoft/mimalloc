8b7781a53976e7df69ad7467738f221ebe19c081
daanx <daan@microsoft.com>
2026-07-30 21:27:03 -0700

    fix aliased mi_malloc_size/mi_malloc_usable_size

--- src/alloc-posix.c
+++ src/alloc-posix.c
@@ -31,17 +31,6 @@ terms of the MIT license. A copy of the license can be found in the file
 #define ENOMEM 12
 #endif
 
-
-mi_decl_nodiscard size_t mi_malloc_size(const void* p) mi_attr_noexcept {
-  // if (!mi_is_in_heap_region(p)) return 0;
-  return mi_usable_size(p);
-}
-
-mi_decl_nodiscard size_t mi_malloc_usable_size(const void *p) mi_attr_noexcept {
-  // if (!mi_is_in_heap_region(p)) return 0;
-  return mi_usable_size(p);
-}
-
 mi_decl_nodiscard size_t mi_malloc_good_size(size_t size) mi_attr_noexcept {
   return mi_good_size(size);
 }
