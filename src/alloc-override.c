88ad3bc0abda1046f20daa55a04cf3d94967cc08
daanx <daan@microsoft.com>
2026-07-30 20:54:37 -0700

    fix redirection of malloc_(usable_)size, #issue 1271, 3.45

--- src/alloc-override.c
+++ src/alloc-override.c
@@ -328,11 +328,11 @@ extern "C" {
 #ifndef MI_OSX_IS_INTERPOSED
   // Forward Posix/Unix calls as well
   void*  reallocf(void* p, size_t newsize) MI_FORWARD2(mi_reallocf,p,newsize)
-  size_t malloc_size(const void* p)        MI_FORWARD1(mi_usable_size,p)
+  size_t malloc_size(const void* p)        MI_FORWARD1(mi_malloc_usable_size,p)
   #if !defined(__ANDROID__) && !defined(__FreeBSD__) && !defined(__DragonFly__)
-  size_t malloc_usable_size(void *p)       MI_FORWARD1(mi_usable_size,p)
+  size_t malloc_usable_size(void *p)       MI_FORWARD1(mi_malloc_usable_size,p)
   #else
-  size_t malloc_usable_size(const void *p) MI_FORWARD1(mi_usable_size,p)
+  size_t malloc_usable_size(const void *p) MI_FORWARD1(mi_malloc_usable_size,p)
   #endif
 
   // No forwarding here due to aliasing/name mangling issues
