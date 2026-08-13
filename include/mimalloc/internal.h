ed3620bcbe7848ffa09a04531f857980037624fb
daanx <daan@microsoft.com>
2026-08-12 21:29:09 -0700

    find test bug on macOS 14

--- include/mimalloc/internal.h
+++ include/mimalloc/internal.h
@@ -754,13 +754,15 @@ static inline mi_page_t* _mi_checked_ptr_page(const void* p) {
   size_t sub_idx;
   const size_t idx = _mi_page_map_index(p, &sub_idx);
   #if MI_MIN_VABITS < MI_INTPTR_BITS
-  //if mi_unlikely(((uintptr_t)p >> MI_MIN_VABITS) != 0) {  
-    const size_t committed_count = mi_atomic_load_relaxed(&pmap->committed_count);
+  const size_t committed_count = mi_atomic_load_relaxed(&pmap->committed_count);    
+  if mi_unlikely(((uintptr_t)p >> MI_MIN_VABITS) != 0) {  
     if mi_unlikely(idx >= committed_count) {
-      fprintf(stderr,"invalid pointer %p, idx: %zu, committed count: %zu, min_vabits: %zu\n", p, idx, committed_count, MI_MIN_VABITS);
       return NULL;
     }
-  //}   
+  }   
+  if mi_unlikely(idx >= committed_count) {
+    fprintf(stderr,"invalid pointer %p, idx: %zu, committed count: %zu, min_vabits: %d\n", p, idx, committed_count, MI_MIN_VABITS);
+  }
   #endif
   mi_assert_internal(idx < mi_atomic_load_relaxed(&pmap->committed_count));
   mi_submap_t const sub = _mi_page_map_at(pmap,idx);
