155fef1313801772eff498f987db70ef9377a088
daanx <daan@microsoft.com>
2026-08-28 15:29:43 -0700

    improve free codegen

--- include/mimalloc/internal.h
+++ include/mimalloc/internal.h
@@ -773,14 +773,14 @@ static inline mi_page_t* _mi_checked_ptr_page(const void* p) {
 // if the page meta data is aligned in front of pages we can find it efficiently
 // without needing to go through the page map (for valid pointers).
 static inline mi_page_t* _mi_aligned_ptr_page0(const void* p) {
-  // const ptrdiff_t page_idx = ((uint8_t*)p - (uint8_t*)page_metas)/MI_ARENA_SLICE_SIZE;
-  const uintptr_t page_idx = ((uintptr_t)p / MI_ARENA_SLICE_SIZE) % (MI_PAGE_META_ALIGNMENT / MI_ARENA_SLICE_SIZE);
   mi_page_t* const page_metas = (mi_page_t*)_mi_align_down_ptr(p,MI_PAGE_META_ALIGNMENT);
+  // const ptrdiff_t page_idx = ((uint8_t*)p - (uint8_t*)page_metas)/MI_ARENA_SLICE_SIZE;
+  const uintptr_t page_idx    = ((uintptr_t)p / MI_ARENA_SLICE_SIZE) % (MI_PAGE_META_ALIGNMENT / MI_ARENA_SLICE_SIZE);
   mi_assert_internal(page_idx >= 0 && page_idx <= MI_PAGE_META_ALIGNED_COUNT);
-  #if MI_ARCH_ARM64
-  mi_page_t* const page = &page_metas[page_idx];
-  #else
+  #if MI_ARCH_X64 || MI_ARCH_X86 || MI_ARCH_RISCV // better code on x64/x86/riscv64
   mi_page_t* const page = (mi_page_t*)((uintptr_t)page_metas | (page_idx * sizeof(mi_page_t)));
+  #else
+  mi_page_t* const page = &page_metas[page_idx];
   #endif
   return page;
   
