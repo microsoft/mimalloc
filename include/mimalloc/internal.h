c977883aee72e10d17827b5e2004b6140e572d76
FilipNur <48691554+FilipNur@users.noreply.github.com>
2026-08-30 13:43:40 +0200

    Incompatible pointer types error fix (Clang on Windows)

--- include/mimalloc/internal.h
+++ include/mimalloc/internal.h
@@ -789,7 +789,7 @@ static inline mi_page_t* _mi_aligned_ptr_page(const void* p) {
       return NULL;
     }
   #endif
-  return mi_atomic_load_acquire(&page->self);
+  return (mi_page_t *)mi_atomic_load_acquire((const uintptr_t *)(&page->self));
 }
 #endif
 
