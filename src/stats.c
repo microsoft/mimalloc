0d673d1c82b5fbcbc79c5d14003ed7c3f25bf6b3
Daan <daanl@outlook.com>
2026-09-02 12:59:21 -0700

    add mi_theap_stats_merge_to_heap

--- src/stats.c
+++ src/stats.c
@@ -636,6 +636,11 @@ bool mi_theap_stats_get(mi_theap_t* theap, mi_stats_t* stats) mi_attr_noexcept {
   return mi_stats_copy(stats, mi_theap_get_stats(theap));
 }
 
+void mi_theap_stats_merge_to_heap(mi_theap_t* theap) mi_attr_noexcept {
+  if (theap == NULL) return;
+  _mi_theap_merge_stats(theap);
+}
+
 static bool mi_cdecl mi_heap_aggregate_visitor(mi_heap_t* heap, void* arg) {
   mi_stats_t* stats = (mi_stats_t*)arg;
   mi_stats_add_into(stats, mi_heap_get_stats(heap));
