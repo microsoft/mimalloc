5d2916adc7d1a76edfdea2798bde6607b944f9f6
Daan <daanl@outlook.com>
2026-09-09 19:40:14 -0700

    fix assertion

--- src/free.c
+++ src/free.c
@@ -615,7 +615,7 @@ static mi_decl_noinline bool mi_check_double_freex(const mi_page_t* page, const 
 // Used for double free checking to avoid checking free lists too frequently
 static inline bool mi_block_could_be_double_free(const mi_page_t* page, const mi_block_t* block) {
   mi_block_t* n = mi_block_nextx(page,block,page->keys);
-  return (!_mi_is_aligned(p,MI_SIZE_SIZE) &&              // quick check: aligned pointer?
+  return (!_mi_is_aligned(block,MI_SIZE_SIZE) &&              // quick check: aligned pointer?
           (n==NULL || mi_page_contains_address(page,n))); // quick check: in the same page or NULL?  
 }
 
