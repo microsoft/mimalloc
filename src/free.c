66e338bcfacf52f93009107687ef538c5ddfac09
Daan <daanl@outlook.com>
2026-09-26 08:23:08 -0700

    restore older mi_free code

--- src/free.c
+++ src/free.c
@@ -39,14 +39,22 @@ static inline void mi_free_block_local(mi_page_t* page, mi_block_t* block, bool 
   _mi_memset_aligned(block, MI_DEBUG_FREED, dbgsize);  
   #endif
   
-  // actual free: push on the local free list fast-path (ordered like this for good codegen)
+  // actual free: push on the local free list fast-path
+  #if MI_ARCH_X64 || MI_ARCH_X86    // use direct 16-bit decrement 
+  mi_block_set_next(page, block, page->local_free);
+  page->local_free = block;
+  const bool is_empty = (--page->xused.le.used_count == 0);
+  #else // otherwise use whole word decrement
   mi_used_t xused = page->xused;
   mi_block_set_next(page, block, page->local_free);
   xused.used_alloc--;
   page->xused = xused;
   page->local_free = block;
+  const bool is_empty = (mi_xused_used_count(xused) == 0);
+  #endif
   mi_assert_internal(mi_page_alloc_count(page) + mi_page_last_used(page) >= mi_page_used(page));
-  if mi_unlikely(mi_xused_used_count(xused) == 0) {  // is used count zero ?
+  
+  if mi_unlikely(is_empty) {  // is used count zero ?
     if (page->retire_expire==0) { // no need to re-retire retired pages (happens when we alloc/free one block repeatedly in an empty page)
       _mi_page_retire(page); 
     }
